5518615966ac4a234e5fe1ddeee7347fd27b236c
Aaron Emery <aemery14@gmail.com>
2015-11-20 11:16:36 -0500

    Update 2015-11-20T16:16:17.727Z

--- app/scripts/models/BlogPosts.js
+++
@@ -1,9 +0,0 @@
-import Post from 'models/Post';
-
-export default Backbone.Collection.extend({
-  model: Post,
-  url: 'http://tiny-lasagna-server.herokuapp.com/collections/messagess',
-  comparator(model) {
-    return  -model.get('createdAt');
-  }
-});
--- app/scripts/models/Post.js
+++
@@ -1,9 +0,0 @@
-export default Backbone.Model.extend({
-  defaults: function(){
-    return {
-      message: "[no message]",
-      username: "[no username]",
-      createdAt: Date.now(),
-    };
-  }
-});
